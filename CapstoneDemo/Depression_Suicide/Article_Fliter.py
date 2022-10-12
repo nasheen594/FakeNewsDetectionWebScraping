d18c6fb548ca0b18c50aecdf5c74965e4b38b0b4
sj-cmd <59848111+sj-cmd@users.noreply.github.com>
2022-10-12 10:47:48 -0400

    Delete CapstoneDemo/Depression_Suicide directory

--- CapstoneDemo/Depression_Suicide/Article_Fliter.py
+++
@@ -1,7 +0,0 @@
-def relatedArticles(article):
-    keywords = ["depression", "suicide", "depress", "suicidal", "Depression", "Suicide", "Suicidal"]
-    for word in keywords:
-        if word in str(article):
-            print(word)
-            return True
-    return False
